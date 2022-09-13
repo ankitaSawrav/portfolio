c6c37aee658e4bdcb197acfefed69713b4383372
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-09-14 09:40:18 +1000

    images url modified

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,8 @@ function Projects () {
         {
             name: "Wordle",
             synopsis: "An implementation of the classic Wordle",
-            // imageurl:"../images/wordle.png",
-            imageurl:"images/wordle.png",
+            imageurl:"../images/wordle.png",
+            // imageurl:"images/wordle.png",
             github:"https://github.com/ankitaSawrav/ga-wordle-project",
             liveSite: "https://ankitasawrav.github.io/ga-wordle-project/wordle.html",
             techStats:'Vanilla Javascript | CSS | HTML'
@@ -16,7 +16,7 @@ function Projects () {
         {
             name: "Notely",
             synopsis: "A simple note taking app for developers and tech people to save and share notes",
-            imageurl:"images/notely.png",
+            imageurl:"../images/notely.png",
             github:"https://github.com/ankitaSawrav/notely-note-taking",
             liveSite: "https://lit-brushlands-78819.herokuapp.com/",
             techStats:'Python | Javascript | CSS | HTML | Flask | Jinja | PostgreSQL'
@@ -24,7 +24,7 @@ function Projects () {
         {
             name: "Plantr",
             synopsis: "A Plant Buddy app to help you take care of your plants made with lots of love from Magdelina,Ellanor and me. Its a single page app designed for users to search the plants and add them to their green house and look at the care details.It displays current weather detail for user",
-            imageurl:"images/plantr.png",
+            imageurl:"../images/plantr.png",
             github:"https://github.com/ankitaSawrav/notely-note-taking",
             liveSite: "https://plntr.herokuapp.com/",
             techStats:'Express | Node.js | PostgreSQL | Rest API'
@@ -32,7 +32,7 @@ function Projects () {
         {
             name: "Yaadein..",
             synopsis: "Yaadein is a memory journal where you can capture your special moments forever. This is a single page react application.",
-            imageurl:"images/yaadein.png",
+            imageurl:"../images/yaadein.png",
             github:"https://github.com/ankitaSawrav/notely-note-taking",
             liveSite: "https://plntr.herokuapp.com/",
             techStats:'React | PostgreSQL | CSS | Cloudinary'
