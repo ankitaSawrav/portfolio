ccd2e6ecd323894aa411bba03dacc84999156c75
Ankita Sawrav <ankita.sawrav@gmail.com>
2024-02-25 03:28:26 +1100

    commented unused code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,42 +4,42 @@ import './Project.css';
 import projectsData from './ProjectsDetails.js';
 
 function Projects () {
-    const projectsDataa = [
-        {
-            name: "Wordle",
-            synopsis: "An implementation of the classic Wordle",
-            // imageurl:"../images/wordle.png",
-            imageurl:"images/wordle.png",
-            github:"https://github.com/ankitaSawrav/ga-wordle-project",
-            liveSite: "https://ankitasawrav.github.io/ga-wordle-project/wordle.html",
-            techStats:'Vanilla Javascript | CSS | HTML'
-        },
-        {
-            name: "Notely",
-            synopsis: "A simple note taking app for developers and tech people to save and share notes",
-            imageurl:"images/notely.png",
-            github:"https://github.com/ankitaSawrav/notely-note-taking",
-            liveSite: "https://lit-brushlands-78819.herokuapp.com/",
-            techStats:'Python | Javascript | CSS | HTML | Flask | Jinja | PostgreSQL'
-        },
-        {
-            name: "Plantr",
-            synopsis: "A Plant Buddy app to help you take care of your plants made with lots of love from Magdelina,Ellanor and me. Its a single page app designed for users to search the plants and add them to their green house and look at the care details.It displays current weather detail for user",
-            imageurl:"images/plantr.png",
-            github:"https://github.com/magdalenamae/plantr",
-            liveSite: "https://plntr.herokuapp.com/",
-            techStats:'Express | Node.js | PostgreSQL | Rest API'
-        },
-        {
-            name: "Yaadein..",
-            synopsis: "Yaadein is a memory journal where you can capture your special moments forever. This is a single page react application.",
-            imageurl:"images/yaadein.png",
-            github:"https://github.com/ankitaSawrav/memoriesapp",
-            liveSite: "https://sheltered-reef-25477.herokuapp.com/",
-            techStats:'React | PostgreSQL | CSS | Cloudinary'
-        },
+    // const projectsDataa = [
+    //     {
+    //         name: "Wordle",
+    //         synopsis: "An implementation of the classic Wordle",
+    //         // imageurl:"../images/wordle.png",
+    //         imageurl:"images/wordle.png",
+    //         github:"https://github.com/ankitaSawrav/ga-wordle-project",
+    //         liveSite: "https://ankitasawrav.github.io/ga-wordle-project/wordle.html",
+    //         techStats:'Vanilla Javascript | CSS | HTML'
+    //     },
+    //     {
+    //         name: "Notely",
+    //         synopsis: "A simple note taking app for developers and tech people to save and share notes",
+    //         imageurl:"images/notely.png",
+    //         github:"https://github.com/ankitaSawrav/notely-note-taking",
+    //         liveSite: "https://lit-brushlands-78819.herokuapp.com/",
+    //         techStats:'Python | Javascript | CSS | HTML | Flask | Jinja | PostgreSQL'
+    //     },
+    //     {
+    //         name: "Plantr",
+    //         synopsis: "A Plant Buddy app to help you take care of your plants made with lots of love from Magdelina,Ellanor and me. Its a single page app designed for users to search the plants and add them to their green house and look at the care details.It displays current weather detail for user",
+    //         imageurl:"images/plantr.png",
+    //         github:"https://github.com/magdalenamae/plantr",
+    //         liveSite: "https://plntr.herokuapp.com/",
+    //         techStats:'Express | Node.js | PostgreSQL | Rest API'
+    //     },
+    //     {
+    //         name: "Yaadein..",
+    //         synopsis: "Yaadein is a memory journal where you can capture your special moments forever. This is a single page react application.",
+    //         imageurl:"images/yaadein.png",
+    //         github:"https://github.com/ankitaSawrav/memoriesapp",
+    //         liveSite: "https://sheltered-reef-25477.herokuapp.com/",
+    //         techStats:'React | PostgreSQL | CSS | Cloudinary'
+    //     },
 
-    ]
+    // ]
     return (
         <div className="Projects-container">
             <div className="page-title">
